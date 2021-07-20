ef722cf6c5c2f8df981249c1f822c37ad6bc0aff
Markcodexq <markcodexq@gmail.com>
2021-07-20 09:27:55 +0500

    add the validation

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ get '/' do
 end
 
 get '/about' do
+	@error = "something wrong!"
 	erb :about
 end
 
@@ -27,11 +28,21 @@ post '/visit' do
 	@choise = params[:choise]
 	@color = params[:color]
 
+	if @user == ""
+		@error = "Write a user name"
+	elsif @phone == ""
+		@error = "Write a phone number"
+	elsif @date == ""
+		@error = "Write the data"
+	end
+
+	if @error != ""
+		return erb :visit
+	end
 	@message = "Dear #{@user}, you will add to our system, phone is - #{@phone}, date - #{@date}, master - #{@choise}, color - #{@color}"
 
 	f = File.open './public/users.txt', 'a'
 	f.write "User: #{@user}, Phone: #{@phone}, Date: #{@date}, Master: #{@choise}, Color: #{@color}\n"
 	f.close
-
 	erb :visit
 end
